b17fbe6a995c6589c504455e25cfb5d3e8f89e1e
Christian Van Cleef <cvanclee@oswego.edu>
2018-10-02 15:52:04 -0400

    small update

---
+++ src/cvc/capstone/test/TestSuite.java
@@ -0,0 +1,14 @@
+package cvc.capstone.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	
+})
+
+public class TestSuite {
+
+}
